12a3c72018fdceb59daff62d46435c463bec0e20
wesinator <5124946+wesinator@users.noreply.github.com>
2020-01-31 11:17:25 -0500

    fix init

--- dragos_portal/__init__.py
+++ dragos_portal/__init__.py
@@ -4,4 +4,4 @@
 __author__ = """Dragos, Inc."""
 __version__ = '0.1.0'
 
-from .dragos_portal import DragosPortalAPI, dragosportal_api_config
+from .dragos_portal import DragosPortalAPI, load_api_config
